c81455431f8ea45c74e65955c2979bfdd699212f
ArnoDevolder <devolderarno@outlook.com>
2021-12-21 18:21:11 +0100

    eerste commit

--- src/app/nuki-design/nuki-reservation/nuki-reservation.component.ts
+++ src/app/nuki-design/nuki-reservation/nuki-reservation.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms'
 import { UserProfile } from 'src/app/app-http-calls/user-data.service';
 import { fade, staggerFade } from '../animations';
+import { UserDataService } from 'src/app/app-http-calls/user-data.service';
 
 
 @Component({
@@ -17,17 +18,28 @@ export class NukiReservationComponent implements OnInit {
 
   reservationFormGroup: FormGroup;
 
-  constructor(private fb: FormBuilder) { 
+  constructor(private fb: FormBuilder, private userService: UserDataService) { 
     this.reservationFormGroup = this.fb.group({
       email: ["", [Validators.required, Validators.email]]
     })
   }
 
+  users!: UserProfile[];
+  emailarray! : string[];
+
   ngOnInit(): void {
     this.reservationFormGroup
     .valueChanges
-    .subscribe(item => 
-      console.log(item as UserProfile))
+    .subscribe(item => {
+      console.log(item)
+      this.userService.getUsers()
+      .subscribe(users => {
+      this.users = users
+      for (let i = 0; i < users.length; i++) {
+        if(users[i].emailAdress == (item.email)){console.log("EXISTS")}
+      }
+    })
+    })
   }
 
   get email(){
